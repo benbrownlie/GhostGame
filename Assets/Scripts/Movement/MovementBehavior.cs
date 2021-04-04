6c9c0271ae2065962d66a75f19ab19efa83071a6
Benjamin Brownlie <benjibspam@gmail.com>
2021-04-04 02:08:57 -0500

    Test Push, added Movement script

---
+++ Assets/Scripts/Movement/MovementBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float MoveForce = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Vertical"))
+            _rigidbody.AddForce(new Vector3(MoveForce, 0, 0), ForceMode.Impulse);
+    }
+}
